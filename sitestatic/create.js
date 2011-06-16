34690bd70254f9bfdab3a685d26fd39bc7d5814b
Zach Schwartz <zischwartz@gmail.com>
2011-06-16 19:57:18 -0400

    meh

--- sitestatic/create.js
+++ sitestatic/create.js
@@ -8,9 +8,9 @@ $("#add_static").click(function(){
 
 $(".a_static_element").click(function(e){
 	element_id= $(this).data("id");
-	 $("#add_static_form").slideDown('fast', function(e){
+	 $("#edit_static_form, #edit_static").slideDown('fast', function(e){
 		// alert(element_id);
-		$("#add_static_form").load(window.location+"/editstatic/"+element_id);
+		$("#edit_static_form").load(window.location+"/editstatic/"+element_id);
 	});
 
 })
