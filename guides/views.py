be8e8b6fc33190106d200fad523eb93ec503bfa6
Zach Schwartz <zischwartz@gmail.com>
2011-06-25 13:09:26 -0400

    fixed silly template naming and heirarchy

--- guides/views.py
+++ guides/views.py
@@ -20,18 +20,18 @@ from log import getlogger
 # -------------------------
 def GuideListView (request):
 	guide_list = Guide.objects.all()
-	return render_to_response("home.html", locals(), context_instance=RequestContext(request))
+	return render_to_response("enjoy/home.html", locals(), context_instance=RequestContext(request))
 
 def GuideDetailView (request, slug):
 	guide = get_object_or_404(Guide, slug=slug)
 	slide_list= guide.slide_set.all()
-	return render_to_response("guide_detail.html", locals(), context_instance=RequestContext(request))
+	return render_to_response("enjoy/guide_detail.html", locals(), context_instance=RequestContext(request))
 
 def SlideDetailView (request, gslug, slug):
 	slide = get_object_or_404(Slide, guide__slug=gslug, slug=slug)
 	static_elements = slide.staticelement_set.all()
 	interactive_elements=slide.interactiveelement_set.all().select_subclasses()
-	return render_to_response("slide.html", locals(), context_instance=RequestContext(request))
+	return render_to_response("enjoy/slide.html", locals(), context_instance=RequestContext(request))
 	
 
 # Creating Guides
