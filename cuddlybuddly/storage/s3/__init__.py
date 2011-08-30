14e533fc0dba3f0a05fdc16851e760ec8221a466
Zach Schwartz <zischwartz@gmail.com>
2011-08-30 12:32:49 -0400

    still cleaning up from the reorganization

--- cuddlybuddly/storage/s3/__init__.py
+++
@@ -1,5 +0,0 @@
-from cuddlybuddly.storage.s3.lib import CallingFormat
-from cuddlybuddly.storage.s3.storage import S3Storage
-
-
-__all__ = ['CallingFormat', 'S3Storage']
--- cuddlybuddly/storage/s3/exceptions.py
+++
@@ -1,2 +0,0 @@
-class S3Error(IOError):
-    pass
